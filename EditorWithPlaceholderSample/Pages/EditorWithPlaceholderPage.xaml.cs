bf2755e488c3ce9d1c0aeb812808c6d581fd5118
June Cho <junecho.ucb@gmail.com>
2017-09-26 20:29:38 -0700

    added EditorWithPlaceholderPage

---
+++ EditorWithPlaceholderSample/Pages/EditorWithPlaceholderPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace EditorWithPlaceholderSample.Pages
+{
+    public partial class EditorWithPlaceholderPage : ContentPage
+    {
+        public EditorWithPlaceholderPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
