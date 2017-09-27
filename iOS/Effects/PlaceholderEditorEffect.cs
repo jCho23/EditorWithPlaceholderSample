9289fab3415b769dba4f27e64e4097a151e7e07b
June Cho <junecho.ucb@gmail.com>
2017-09-26 20:41:25 -0700

    added Assembly

---
+++ iOS/Effects/PlaceholderEditorEffect.cs
@@ -0,0 +1,19 @@
+﻿using UIKit;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+using EditorWithPlaceholderSample.iOS.Effects;
+
+
+[assembly: ResolutionGroupName("Xamarin")]
+[assembly: ExportEffect(typeof(PlaceholderEditorEffect), nameof(PlaceholderEditorEffect))]
+namespace EditorWithPlaceholderSample.iOS.Effects
+{
+    public class PlaceholderEditorEffect
+    {
+        public PlaceholderEditorEffect()
+        {
+        }
+    }
+}
