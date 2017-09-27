fae53c93cd4fb359280f4840d922f56e1a87116b
June Cho <junecho.ucb@gmail.com>
2017-09-26 20:37:19 -0700

    Added Placeholder Effect

--- EditorWithPlaceholderSample/Controls/EditorWithPlaceholder.cs
+++ EditorWithPlaceholderSample/Controls/EditorWithPlaceholder.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using Xamarin.Forms;
+
 namespace EditorWithPlaceholderSample.Controls
 {
-    public class EditorWithPlaceholder
+
+    public class EditorWithPlaceholder : Editor
     {
         public EditorWithPlaceholder()
         {
+            Effects.Add(Effect.Resolve("Xamarin.PlaceholderEditorEffect"));
         }
+
+        public string Placeholder { get; set; }
+        public Color PlaceholderTextColor { get; set; } = Color.LightGray;
     }
 }
