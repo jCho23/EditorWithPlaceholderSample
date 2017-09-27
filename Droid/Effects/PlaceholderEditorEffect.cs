6e81dbc749f457e15fe0de960b08d941162472b7
June Cho <junecho.ucb@gmail.com>
2017-09-26 20:58:30 -0700

    Added OnAttached Droid

--- Droid/Effects/PlaceholderEditorEffect.cs
+++ Droid/Effects/PlaceholderEditorEffect.cs
@@ -24,6 +24,38 @@ namespace EditorWithPlaceholderSample.Droid.Effects
 		EditText nativeTextField;
 		EditorWithPlaceholder formsEditor;
 
+		protected override void OnAttached()
+		{
+			formsEditor = Element as EditorWithPlaceholder;
+			nativeTextField = Control as EditText;
 
+			try
+			{
+				placeholderText = new TextView(Forms.Context)
+				{
+					Text = formsEditor.Placeholder,
+
+				};
+				placeholderText.SetBackgroundColor(Android.Graphics.Color.Transparent);
+				placeholderText.SetTextColor(formsEditor.PlaceholderTextColor.ToAndroid());
+				placeholderText.SetLines(1);
+
+
+				backgroundColor = Android.Graphics.Color.White;
+				Control.SetBackgroundColor(backgroundColor);
+
+				if (string.IsNullOrEmpty(formsEditor.Text))
+					placeholderText.Alpha = 1;
+				else
+					placeholderText.Alpha = 0;
+
+
+				////nativeTextField.Add(placeholderText);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+			}
+		}
     }
 }
