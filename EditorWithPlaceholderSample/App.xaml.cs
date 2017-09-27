353108af660dd1acd8b8f00f527efd7e17aa108c
June Cho <junecho.ucb@gmail.com>
2017-09-26 20:46:29 -0700

    Build 1

--- EditorWithPlaceholderSample/App.xaml.cs
+++ EditorWithPlaceholderSample/App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Xamarin.Forms;
 
+using EditorWithPlaceholderSample.Pages;
+
 namespace EditorWithPlaceholderSample
 {
     public partial class App : Application
@@ -8,7 +10,7 @@ namespace EditorWithPlaceholderSample
         {
             InitializeComponent();
 
-            MainPage = new EditorWithPlaceholderSamplePage();
+            MainPage = new EditorWithPlaceholderPage();
         }
 
         protected override void OnStart()
