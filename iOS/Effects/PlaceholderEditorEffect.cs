816ef92f4f0d0ce998b523c41b128bb1344a175a
June Cho <junecho.ucb@gmail.com>
2017-09-26 20:43:27 -0700

    Added OnAttached

--- iOS/Effects/PlaceholderEditorEffect.cs
+++ iOS/Effects/PlaceholderEditorEffect.cs
@@ -4,16 +4,43 @@ using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
 using EditorWithPlaceholderSample.iOS.Effects;
-
+using EditorWithPlaceholderSample.Controls;
 
 [assembly: ResolutionGroupName("Xamarin")]
 [assembly: ExportEffect(typeof(PlaceholderEditorEffect), nameof(PlaceholderEditorEffect))]
 namespace EditorWithPlaceholderSample.iOS.Effects
 {
-    public class PlaceholderEditorEffect
+
+    public class PlaceholderEditorEffect : PlatformEffect
     {
-        public PlaceholderEditorEffect()
+        UILabel placeholderText;
+        UITextView nativeTextField;
+        EditorWithPlaceholder formsEditor;
+
+        protected override void OnAttached()
         {
+            formsEditor = Element as EditorWithPlaceholder;
+            nativeTextField = Control as UITextView;
+
+            placeholderText = new UILabel
+            {
+                LineBreakMode = UILineBreakMode.WordWrap,
+                Lines = 1,
+                BackgroundColor = UIColor.Clear,
+                TextColor = formsEditor.PlaceholderTextColor.ToUIColor(),
+                Text = formsEditor.Placeholder
+            };
+
+            if (string.IsNullOrEmpty(formsEditor.Text))
+                placeholderText.Alpha = 1;
+            else
+                placeholderText.Alpha = 0;
+
+            nativeTextField.AddSubview(placeholderText);
         }
+
+
+
+
     }
 }
